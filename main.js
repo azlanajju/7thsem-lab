ef556b4a71c2b5ee559949fb152bca30799a8ba3
muhammedazlan11@gmail.com <muhammedazlan11@gmail.com>
2022-10-15 11:33:22 +0530

    new

---
+++ main.js
@@ -0,0 +1,213 @@
+
+  jSuites.notification({
+        name: 'WELCOME',
+        message: '           -by azlan',
+    });  
+
+
+function myFunction8() {
+  
+  jSuites.notification({
+        name: 'copied',
+        message:'program1'
+        
+    }); 
+ document.getElementById("demo").innerHTML = "Copied!";
+
+  var copyText = document.getElementById("myInput");
+  copyText.select(); 
+  navigator.clipboard.writeText(copyText.value);
+  
+}
+
+function myFunction7() {
+  
+  jSuites.notification({
+        name: 'copied',
+        message:'program2',
+        
+        
+    }); 
+ document.getElementById("demo1").innerHTML = "Copied!";
+  var copyText = document.getElementById("myInput1");
+ copyText.select(); 
+ 
+  navigator.clipboard.writeText(copyText.value); 
+ 
+  
+}
+
+function myFunction6() {
+  
+  jSuites.notification({
+        name: 'copied',
+        message:'program3'
+        
+    }); 
+ document.getElementById("demo2").innerHTML = "Copied!";
+  var copyText = document.getElementById("myInput2");
+  copyText.select();
+  
+  navigator.clipboard.writeText(copyText.value);
+  
+ 
+  
+}
+
+function myFunction5() {
+  
+  jSuites.notification({
+        name: 'copied',
+        message:'program4'
+        
+    }); 
+  
+ document.getElementById("demo3").innerHTML = "Copied!";
+  var copyText = document.getElementById("myInput3");
+  copyText.select();
+  
+  navigator.clipboard.writeText(copyText.value);
+ 
+  
+}
+
+function myFunction4() {
+  
+  jSuites.notification({
+        name: 'copied',
+        message:'program5'
+        
+    }); 
+ document.getElementById("demo4").innerHTML = "Copied!";
+  var copyText = document.getElementById("myInput4");
+  copyText.select();
+  
+  navigator.clipboard.writeText(copyText.value);
+ 
+  
+}
+
+function myFunction3() {
+  
+  jSuites.notification({
+        name: 'copied',
+        message:'program6'
+        
+    }); 
+ document.getElementById("demo5").innerHTML = "Copied!";
+  var copyText = document.getElementById("myInput5");
+  copyText.select();
+  
+  navigator.clipboard.writeText(copyText.value);
+ 
+  
+}
+
+function myFunction2() {
+  
+  jSuites.notification({
+        name: 'copied',
+        message:'program7'
+        
+    }); 
+ document.getElementById("demo6").innerHTML = "Copied!";
+  var copyText = document.getElementById("myInput6");
+  copyText.select();
+  
+  navigator.clipboard.writeText(copyText.value);
+ 
+  
+}
+
+function myFunction1() {
+  
+  jSuites.notification({
+        name: 'copied',
+        message:'program8'
+        
+    }); 
+ document.getElementById("demo7").innerHTML = "Copied!";
+  var copyText = document.getElementById("myInput7");
+  copyText.select();
+  
+  navigator.clipboard.writeText(copyText.value);
+ 
+  
+}
+
+function myFunction0() {
+  
+ document.getElementById("demo8").innerHTML = "Copied!";
+  var copyText = document.getElementById("myInput9");
+  copyText.select();
+  
+  navigator.clipboard.writeText(copyText.value);
+ 
+  
+} 
+
+function myFunction9() {
+  
+  jSuites.notification({
+        name: 'copied',
+        message:'program9'
+        
+    }); 
+ document.getElementById("demo9").innerHTML = "Copied!";
+
+  var copyText = document.getElementById("myInput10");
+  copyText.select(); 
+  navigator.clipboard.writeText(copyText.value);
+
+  
+}
+
+function myFunction10() {
+  
+  jSuites.notification({
+        name: 'copied',
+        message:'program9'
+        
+    }); 
+ document.getElementById("demo10").innerHTML = "Copied!";
+
+  var copyText = document.getElementById("myInput11");
+  copyText.select(); 
+  navigator.clipboard.writeText(copyText.value);
+
+  
+}
+
+
+function myFunction11() {
+  
+  jSuites.notification({
+        name: 'copied',
+        message:'program12'
+        
+    }); 
+ document.getElementById("demo11").innerHTML = "Copied!";
+
+  var copyText = document.getElementById("myInput12");
+  copyText.select(); 
+  navigator.clipboard.writeText(copyText.value);
+
+  
+}
+
+
+function myFunction12() {
+  
+  jSuites.notification({
+        name: 'copied',
+        message:'program13'
+        
+    }); 
+ document.getElementById("demo12").innerHTML = "Copied!";
+
+  var copyText = document.getElementById("myInput13");
+  copyText.select(); 
+  navigator.clipboard.writeText(copyText.value);
+
+  
+}
